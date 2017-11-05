eb5233d0bb896e63deb3b5374b3506526da677d3
theheroGAC <gamesandconsolestaff@gmail.com>
2017-11-05 08:50:20 +0100

    Update shared.lua

--- qlinstaller/git/shared.lua
+++ qlinstaller/git/shared.lua
@@ -1,7 +1,7 @@
 -- Constants
 
 APP_REPO = "theheroGAC"
-APP_PROJECT = "QuickLaunch_installer_for_PSVita"			--here name of repo in github...your vpk is QuickLaunch_installer_for_PSVita.vpk
+APP_PROJECT = "QuickLaunch_installer_fork_for_PSVita"			--here name of repo in github...your vpk is QuickLaunch_installer_for_PSVita.vpk
 
 APP_VERSION_MAJOR = 0x00 -- major.minor
 APP_VERSION_MINOR = 0x04
